76949098bc35eb553e83a26ae977b55ff5d7df3d
alexandreribeiro04022035-arch <alexandreribeiro04022035@gmail.com>
2025-12-17 22:08:32 -0300

    Update gerar_json.py

--- gerar_json.py
+++ gerar_json.py
@@ -2,8 +2,8 @@ import os
 import json
 import requests
 
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+SUPABASE_URL = os.getenv("https://lpkhscatjrllfscqmxka.supabase.co ")
+SUPABASE_KEY = os.getenv("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxwa2hzY2F0anJsbGZzY3FteGthIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM1NzU3MjYsImV4cCI6MjA3OTE1MTcyNn0.HlNIZFU2kq2-pyq0PgBxFX1Kg1iKldF_Y3thWKzYBnM")
 
 TABLE = "banco"
 OUTPUT_FILE = "cache/banco.json"
