cbf23b8e9767515d654da38106f6a16d74a1acbe
alexandreribeiro04022035-arch <alexandreribeiro04022035@gmail.com>
2025-12-18 17:40:27 -0300

    Create gerar_cache.py

---
+++ gerar_cache.py
@@ -0,0 +1,26 @@
+import os, json, requests
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+URL = f"{SUPABASE_URL}/rest/v1/banco?select=id,nome,email,saldo,credito,debito"
+
+headers = {
+    "apikey": SUPABASE_KEY,
+    "Authorization": f"Bearer {SUPABASE_KEY}",
+    "Content-Type": "application/json"
+}
+
+def gerar_cache():
+    r = requests.get(URL, headers=headers)
+
+    if r.status_code != 200:
+        raise Exception(r.text)
+
+    dados = r.json()
+
+    os.makedirs("cache", exist_ok=True)
+    with open("cache/banco.json", "w", encoding="utf-8") as f:
+        json.dump(dados, f, ensure_ascii=False, indent=2)
+
+    return len(dados)
